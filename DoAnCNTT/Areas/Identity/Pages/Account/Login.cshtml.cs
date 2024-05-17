efc3e9f88eaacf917ffa66c480b56c05b43e234b
lethai3979 <lethai3979@gmail.com>
2024-05-17 20:19:31 +0700

    Fix authorize

--- DoAnCNTT/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DoAnCNTT/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -105,7 +105,8 @@ namespace DoAnCNTT.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            //returnUrl ??= Url.Content("~/");
+            returnUrl = "~/Home/Index";
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
