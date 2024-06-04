a68b782ce6eac84797093fbd31038aea173417bf
lethai3979 <lethai3979@gmail.com>
2024-06-04 22:11:46 +0700

    fix employees controller route

--- DoAnCNTT/Areas/Admin/Controllers/EmployeesController.cs
+++ DoAnCNTT/Areas/Admin/Controllers/EmployeesController.cs
@@ -1,26 +1,169 @@
-﻿using DoAnCNTT.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoAnCNTT.Data;
 using DoAnCNTT.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DoAnCNTT.Areas.Admin.Controllers
 {
-	public class EmployeesController : Controller
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly ApplicationDbContext _context;
+    [Area("Admin")]
+    public class EmployeesController : Controller
+    {
 
-		public EmployeesController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
-		{
-			_userManager = userManager;
-			_context = context;
-		}
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
 
-		// GET: CustomerController
-		public async Task<ActionResult> Index()
-		{
-			var employee = await _userManager.GetUsersInRoleAsync("Employee");
-			return View(employee);
-		}
-	}
+        public EmployeesController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        // GET: CustomerController
+        public async Task<ActionResult> Index()
+        {
+            var employee = await _userManager.GetUsersInRoleAsync("Employee");
+            return View(employee);
+        }
+
+        // GET: Admin/Reports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report
+                .Include(r => r.Post)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // GET: Admin/Reports/Create
+        public IActionResult Create()
+        {
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id");
+            return View();
+        }
+
+        // POST: Admin/Reports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Content,PostId,Id,CreatedById,CreatedOn,ModifiedById,ModifiedOn,IsDeleted")] Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(report);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", report.PostId);
+            return View(report);
+        }
+
+        // GET: Admin/Reports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report.FindAsync(id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", report.PostId);
+            return View(report);
+        }
+
+        // POST: Admin/Reports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Content,PostId,Id,CreatedById,CreatedOn,ModifiedById,ModifiedOn,IsDeleted")] Report report)
+        {
+            if (id != report.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(report);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReportExists(report.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Id", report.PostId);
+            return View(report);
+        }
+
+        // GET: Admin/Reports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var report = await _context.Report
+                .Include(r => r.Post)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return View(report);
+        }
+
+        // POST: Admin/Reports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var report = await _context.Report.FindAsync(id);
+            if (report != null)
+            {
+                _context.Report.Remove(report);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReportExists(int id)
+        {
+            return _context.Report.Any(e => e.Id == id);
+        }
+    }
 }
